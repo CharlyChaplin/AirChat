c48e7394df0d4fcd8780e2bb366f7b196b7d4c51
CharlyChaplin <charlychaplin83@gmail.com>
2024-06-16 15:40:05 +0700

    Добавлено меню в шапку чата

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,7 +1,14 @@
 const chats = document.querySelectorAll('.chatItem');
+// toggle Gag and Content
 const gag = document.querySelector('.content__gag');
 const chatContent = document.querySelector('.content__wrapper');
+// work with Burger-menu
+const burgerIcon = document.querySelector('.menu-icon');
+const burgerIconInnerWrapper = document.querySelector('.menu-icon__wrapper');
+const burgerMenu = document.querySelector('.menu');
 
+
+// Handle toggle Gag page/Content page
 chats.forEach(chat => chat.addEventListener('click', () => addActiveClass(chat)));
 
 function addActiveClass(elem) {
@@ -15,3 +22,20 @@ function removeActiveClass() {
 	chats.forEach(chat => chat.classList.remove('chatItem--active'));
 }
 
+
+// Menu Handle Open
+burgerIcon.addEventListener('click', handleMenuOpen);
+
+function handleMenuOpen(e) {
+	e.stopPropagation();
+	burgerIcon.classList.add('menu-icon--active');
+	burgerMenu.classList.remove('menu--hidden');
+	window.addEventListener('click', e => handleCloseMenu(e), { once: true });
+}
+
+function handleCloseMenu(e) {
+	if (e.target !== burgerIcon && e.target !== burgerIconInnerWrapper) {
+		burgerMenu.classList.add('menu--hidden');
+		burgerIcon.classList.remove('menu-icon--active');
+	}
+}
