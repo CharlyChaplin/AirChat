07319fde1b9c8a6c06289c2766c43a0a4f2a8a77
CharlyChaplin <charlychaplin83@gmail.com>
2024-06-16 12:49:22 +0700

    добавлена шапка в выбранный чат

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -1,10 +1,14 @@
 const chats = document.querySelectorAll('.chatItem');
+const gag = document.querySelector('.content__gag');
+const chatContent = document.querySelector('.content__wrapper');
 
 chats.forEach(chat => chat.addEventListener('click', () => addActiveClass(chat)));
 
 function addActiveClass(elem) {
 	removeActiveClass();
 	elem.classList.add('chatItem--active');
+	gag.classList.add('content__gag--hidden');
+	chatContent.classList.remove('content__wrapper--hidden');
 }
 
 function removeActiveClass() {
