f88d597939cd7ba52a6ca7a70dfd66603f5e6dc4
CharlyChaplin <charlychaplin83@gmail.com>
2024-06-18 14:34:04 +0700

    Сделано меню для прикрепления данных к сообщению + правка багов

--- src/utils/chat.js
+++ src/utils/chat.js
@@ -2,11 +2,20 @@ const chats = document.querySelectorAll('.chatItem');
 // toggle Gag and Content
 const gag = document.querySelector('.content__gag');
 const chatContent = document.querySelector('.content__wrapper');
+
 // work with Burger-menu
 const burgerIcon = document.querySelector('.menu-icon');
-const burgerIconInnerWrapper = document.querySelector('.menu-icon__wrapper');
 const burgerMenu = document.querySelector('.menu');
+
+// work with menu-attach
+const attachIcon = document.querySelector('.attach-icon');
+const attachMenu = document.querySelector('.menu-attach');
+
+// autoFocus on Input
 const msgBox = document.querySelector('.content__msg-input');
+// addEventListener("DOMContentLoaded", e => {
+// 	console.log("Dom loaded");
+// })
 
 
 // Handle toggle Gag page/Content page
@@ -26,22 +35,39 @@ function removeActiveClass() {
 
 
 // Menu Handle Open
-burgerIcon.addEventListener('click', handleMenuOpen);
+[burgerIcon, attachIcon].forEach(el => {
+	el.addEventListener('click', handleMenuOpen);
+});
 
 function handleMenuOpen(e) {
+	console.log('e.currentTarget=', e.currentTarget);
 	e.stopPropagation();
-	burgerIcon.classList.add('menu-icon--active');
-	burgerMenu.classList.remove('menu--hidden');
+	if (e.currentTarget === burgerIcon) {
+		if (attachIcon.classList.contains('attach-icon--active')) {
+			handleCloseMenu(burgerIcon);
+		}
+		burgerIcon.classList.add('menu-icon--active');
+		burgerMenu.classList.remove('menu--hidden');
+	} else if (e.currentTarget === attachIcon) {
+		if (burgerIcon.classList.contains('menu-icon--active')) {
+			handleCloseMenu(attachIcon);
+		}
+		attachIcon.classList.add('attach-icon--active');
+		attachMenu.classList.remove('menu-attach--hidden');
+	}
+
 	window.addEventListener('click', e => handleCloseMenu(e), { once: true });
 }
 
+// Menu Handle Close
 function handleCloseMenu(e) {
-	if (e.target !== burgerIcon && e.target !== burgerIconInnerWrapper) {
-		burgerMenu.classList.add('menu--hidden');
+	// console.log('e.target=', e.target, '\ne.currentTarget=', e.currentTarget);
+	if (e.target !== burgerIcon) {
 		burgerIcon.classList.remove('menu-icon--active');
+		burgerMenu.classList.add('menu--hidden');
+	}
+	if (e.target !== attachIcon) {
+		attachIcon.classList.remove('attach-icon--active');
+		attachMenu.classList.add('menu-attach--hidden');
 	}
 }
-
-addEventListener("DOMContentLoaded", e => {
-	console.log("Dom loaded");
-})
