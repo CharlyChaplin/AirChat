dc13f9d0287f5c631b358118a631164026982325
CharlyChaplin <charlychaplin83@gmail.com>
2024-06-21 15:17:38 +0700

    Добавлена возможность изменения/вставки аватара

---
+++ src/components/Profile/UserLogo/addAvatar.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.userIcon__input');
+const userIcon = document.querySelector('.userIcon__icon');
+const overlay = document.querySelector('.userIcon__overlay');
+const previewImage = document.querySelector('.userIcon__img');
+
+overlay.addEventListener('click', e => {
+	e.stopPropagation();
+	input.click();
+})
+
+input.addEventListener('input', e => {
+	const file = e.target.files[0];
+
+	createImg(file);
+	userIcon.style.cssText = `display: none`;
+	previewImage.style.cssText = `display: flex`;
+});
+
+function createImg(file) {
+	previewImage.innerHTML = `<img src=${URL.createObjectURL(file)} alt="AvaIco">`;
+	URL.revokeObjectURL(file);
+}
