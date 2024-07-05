d8752f24112dd945090dc612c71602a44a80ee69
CharlyChaplin <charlychaplin83@gmail.com>
2024-07-05 21:26:20 +0700

    Доработан логотип

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -15,49 +15,7 @@ function removeFromClassList(clsString = "", subStr = '') {
 	}
 }
 
-function updateSuperellipse() {
-	const generateSuperellipsePath = (a, b, n, points = 100) => {
-		let path = '';
-		for (let i = 0; i <= points; i++) {
-			const theta = (Math.PI * 2 * i) / points;
-			const x = Math.pow(Math.abs(Math.cos(theta)), 2 / n) * a * Math.sign(Math.cos(theta));
-			const y = Math.pow(Math.abs(Math.sin(theta)), 2 / n) * b * Math.sign(Math.sin(theta));
-			if (i === 0) {
-				path += `M ${x + a} ${y + b} `;
-			} else {
-				path += `L ${x + a} ${y + b} `;
-			}
-		}
-		path += 'Z';
-		return path;
-	}
-	
-	const svg = document.querySelector('.logo > svg');
-	const container = svg.parentElement;
-	const width = container.clientWidth;
-	const height = container.clientHeight;
-	const minDimension = Math.min(width, height);
-	const a = minDimension / 2;
-	const b = minDimension / 2;
-	const n = 5.5; // параметр суперэллипса, который определяет его форму
-	const pathData = generateSuperellipsePath(a, b, n);
-
-	svg.setAttribute('viewBox', `0 0 ${minDimension} ${minDimension}`);
-	svg.innerHTML = `<path d="${pathData}" />`;
-
-
-}
-
-function initSquircle() {
-	for (let evt of ['resise', 'load']) {
-		window.addEventListener(evt, updateSuperellipse);
-	}
-}
-
-
-initSquircle();
 
 module.exports = {
-	removeFromClassList,
-	updateSuperellipse
+	removeFromClassList
 }
