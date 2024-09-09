fa77c41979ec6cca1012108e38612b8c3a0d7e13
CharlyChaplin <charlychaplin83@gmail.com>
2024-09-09 14:42:19 +0700

    настройка eslist

--- src/utils/mydash/rangeRight.js
+++ src/utils/mydash/rangeRight.js
@@ -37,13 +37,13 @@ function rangeRight(from, to, step = 1) {
 	return result;
 }
 
-console.log(rangeRight(4));				// [ 4, 3, 2, 1 ]
-console.log(rangeRight(-4));				// [ -4, -3, -2, -1 ]
-console.log(rangeRight(1, 5));			// [ 5, 4, 3, 2 ]
-console.log(rangeRight(0, 20, 5));		// [ 20, 15, 10, 5 ]
-console.log(rangeRight(0, -4, -1));		// [ -4, -3, -2, -1 ]
-console.log(rangeRight(1, 4, 0));		// [ 1, 1, 1 ]
-console.log(rangeRight(0));				// []
+// console.log(rangeRight(4));				// [ 4, 3, 2, 1 ]
+// console.log(rangeRight(-4));				// [ -4, -3, -2, -1 ]
+// console.log(rangeRight(1, 5));			// [ 5, 4, 3, 2 ]
+// console.log(rangeRight(0, 20, 5));		// [ 20, 15, 10, 5 ]
+// console.log(rangeRight(0, -4, -1));		// [ -4, -3, -2, -1 ]
+// console.log(rangeRight(1, 4, 0));		// [ 1, 1, 1 ]
+// console.log(rangeRight(0));				// []
 
 
 
